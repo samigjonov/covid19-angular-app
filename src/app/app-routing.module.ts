79f0f5b6e81a15d5d8bf7b6af7464e6b475a51d5
Ulugbek Samigjonov <samigjonov@gmail.com>
2021-01-31 07:15:14 +0500

    6-dars: davlatning statistikasini yuklab olish qushildi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'country',
+    path: 'country/:code',
     component: CountryPageComponent
   }
 ];
--- src/app/country-page/country-page.component.ts
+++ src/app/country-page/country-page.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-page.component.scss']
 })
 export class CountryPageComponent implements OnInit {
-
-  constructor() { }
+  countryStatistics;
+  constructor(private route: ActivatedRoute,
+              private httpClient: HttpClient) {
+    const countryCode = this.route.snapshot.params.code;
+    this.httpClient.get('https://disease.sh/v3/covid-19/countries/' + countryCode).subscribe(result => {
+      this.countryStatistics = result;
+    });
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
